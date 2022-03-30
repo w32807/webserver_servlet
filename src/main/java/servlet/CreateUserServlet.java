626236ca191c2d11b21f3bf66783031a5c8e8f52
w32807 <w32807@nate.com>
2022-03-30 21:20:05 +0900

    Add Servlet

---
+++ src/main/java/servlet/CreateUserServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+
+import db.DataBase;
+import dto.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/create")
+public class CreateUserServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User(
+                req.getParameter("userId"),
+                req.getParameter("password"),
+                req.getParameter("name"),
+                req.getParameter("email"));
+        DataBase.addUser(user);
+        resp.sendRedirect("/user/list");
+    }
+}
